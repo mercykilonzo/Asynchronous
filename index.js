bc46a277f5221a3fbfc8273c63b3d554fb645793
Mercy Mwikali <mwikalimercykilonzo.com>
2025-05-29 13:31:45 +0300

    redefined the pseudocode

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // 1.
 // Create a function that takes in the email as an argument 
 // create a variable to hold the promise
-// Using setTimeout, set a waiting time of 5 seconds to delay the execution of the code by 5 seconds
+// Using setTimeout, set a waiting time of 5 seconds to delay the execution
 // Create another function to simulate the reminders
-// Declare a list of emails that holds the emails of the 3 users.
 // Loop through the list of emails and send a reminder to each email
 async function sendReminder(email) {
     let promise = new Promise((resolve) => {
@@ -23,7 +22,14 @@ async function sendRemindersToAll(users) {
 const users = ['alice@gmail.com', 'mercy@gmail.com', 'joy@gmail.com'];
 sendRemindersToAll(users);
 
-const tryLogin = 3;
+// 2.
+// Initialize attempts to 0
+//  create a function loginattempts,
+// Create a new Promise,Inside the Promise,  Set a timeout for 1000 milliseconds
+// If attempts <=3:Log "Attempt [attempts] failed"Else if attempts === 3:Log "Login successful"
+// Increment attempts by 1
+
+const tryLogin = 0;
 const tryLoginPromise = new Promise((resolve, reject) => {
     if (tryLogin <= 3) {
         setTimeout(() => {
@@ -54,10 +60,14 @@ const tryLoginAsync = async () => {
 };
 tryLoginAsync()
 
-3.
+// 3.
+// Set variable counter = 5
+// Create interval using setInterval that triggers every 1000 milliseconds
+// 4. Inside interval,log current counter value and decrease counter by 1
+// If counter >= 0,Clear the interval and log "Time's up!"
 let count = 5;
 const countdown = setInterval(() => {
-    if (count > 0) {
+    if (count >=0) {
         console.log(`${count}`);
     } else {
         console.log("Time's up!");
@@ -66,16 +76,21 @@ const countdown = setInterval(() => {
     count--;
 }, 1000);
 
-
+// 4.
+// Create a promise with setTimeout to return the waiting period
+// Create an async function loadpage to log "Loading header..." and wait for 1 second
+// After the first log, wait for 2 seconds to log "Loading content..." and a second to log "Loading footer..."
+// After all waits run, log "page fully loaded"
 
 
 
 const wait = (mseconds) => {
-  return new Promise((resolve) => {
+const waitingperiod = new Promise((resolve) => {
     setTimeout(() => {
       resolve();
     }, mseconds);
-  });
+  })
+  return waitingperiod
 };
 const loadPage = async () => {
   try {
@@ -92,7 +107,12 @@ const loadPage = async () => {
 };
 loadPage();
 
-
+// 5.
+// Create a function fetchPrice that takes in symbol as an argument 
+// return a promise after 2 seconds with a resolve message
+// Create an async function that calls fetchPrice with
+// Loop through the syombol arry
+// log the results once they are resolved
 
 function fetchPrice(symbol) {
      return new Promise((resolve) => {
